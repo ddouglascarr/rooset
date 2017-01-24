86f666ef76c6abaac7ad717d33270bf579ebc221
Daniel Carr <d.douglas.carr@gmail.com>
2017-01-24 15:25:21 +1100

    main.cpp
      - Get event store config from env variables
      - Output SystemException json on all caught non ProjectException exceptions
      - Created SystemException

--- rooset-domain/main.cpp
+++ rooset-domain/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <memory>
+#include <cstdlib>
 #include "rapidjson/document.h"
 #include "framework/ProjectException.h"
 #include "framework/JsonUtils.h"
@@ -11,6 +12,7 @@
 #include "framework/IdUtils.h"
 #include "framework/HttpToolsCurlImpl.h"
 #include "aggregates/CommandHandler.h"
+#include "exceptions/SystemException.h"
 
 using namespace std;
 using namespace rooset;
@@ -23,12 +25,19 @@ int main()
     string sJson;
     string buffer;
     while(getline(cin, buffer)) { sJson += buffer; }
+    const char* eventStoreHost = std::getenv("ROOSET_EVENT_STORE_HOST");
+    const char* eventStorePort = std::getenv("ROOSET_EVENT_STORE_PORT");
+
+    if (eventStoreHost == nullptr) throw invalid_argument(
+        "$ROOSET_EVENT_STORE_HOST must be set");
+    if (eventStorePort == nullptr) throw invalid_argument(
+        "$ROOSET_EVENT_STORE_PORT must be set");
 
     shared_ptr<EventRepository> eventRepository = make_shared<
         EventRepositoryGetEventStoreHttpImpl> (
         make_shared<IdToolsImpl>(),
         make_shared<HttpToolsCurlImpl>(),
-        "localhost", "2113");
+        eventStoreHost, eventStorePort);
 
     CommandHandler commandHandler(eventRepository);
     CommandServiceImpl<CommandHandler> commandService(eventRepository, commandHandler);
@@ -41,7 +50,11 @@ int main()
     cout << "\n" << JsonUtils::serialize(*e.serialize()) << "\n";
     return 1;
   } catch (const exception& e) {
-    cout << "\n" << e.what() << "\n";
+    
+    SystemException systemException(
+        ExceptionCode::GENERAL_PROJECT_EXCEPTION,
+        e.what());
+    cout << "\n" << JsonUtils::serialize(*systemException.serialize()) << "\n";
     return 1;
   }
 }
