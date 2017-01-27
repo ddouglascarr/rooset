a2911cf466b8df6d0f9f52e4c0c05e3bb62f28c8
Daniel Carr <d.douglas.carr@gmail.com>
2017-01-27 11:16:19 +1100

    spring initializer project added

---
+++ rooset-http-api/src/main/java/org/rooset/httpapi/RoosetHttpApiApplication.java
@@ -0,0 +1,12 @@
+package org.rooset.httpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoosetHttpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoosetHttpApiApplication.class, args);
+	}
+}
