88fceb68a155bbe66974620a255325aa969039c1
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-03 17:21:37 +1100

    add try catch to filename map

--- rooset-application-toolkit/ratk-declarations-utils/src/generateSchemas.js
+++ rooset-application-toolkit/ratk-declarations-utils/src/generateSchemas.js
@@ -27,51 +27,58 @@ module.exports = (baseSchema, messageCategory, _srcPath) => {
 function mapSchemaFiles(baseSchema, messageCategory) {
   const isException = messageCategory === 'exceptions';
   return (filename) => {
-    const messageSchema = isYamlFilename(filename) ?
-        YAML.load(filename) : require(filename);
-    const type = typeRe.exec(filename)[0];
-    const required = [];
-    const payload = Object.keys(messageSchema.properties)
-        .reduce((memo, v) => {
-          const declaration = messageSchema.properties[v];
-          if (!declaration) throw new Error(`${filename}: no declaration`);
-          const refKey = declaration.type;
-          if (!refKey) {
-            console.log(messageSchema);
-            throw new Error(`${filename}: ${v} has not type property. It is required`);
-          }
-          const ref = baseSchema.definitions[refKey];
-          if (!ref) throw new Error(`${filename}: no defintion for ${refKey}`);
-          required.push(v);
-
-          return merge({}, memo, {
-            definitions: {
-              [refKey]: ref,
-            },
-            properties: {
-              [v]: { '$ref': `#/definitions/${refKey}` },
+    try {
+      const messageSchema = isYamlFilename(filename) ?
+          YAML.load(filename) : require(filename);
+      const type = typeRe.exec(filename)[0];
+      const required = [];
+      const payload = Object.keys(messageSchema.properties)
+          .reduce((memo, v) => {
+            const declaration = messageSchema.properties[v];
+            const refKey = declaration.type;
+            if (!refKey) {
+              console.log(messageSchema);
+              throw new Error(`${filename}: ${v} has not type property. It is required`);
             }
-          });
-        }, { definitions: {}, properties: {} });
-    payload.required = required;
+            const ref = baseSchema.definitions[refKey];
+            if (!ref) throw new Error(`${filename}: no defintion for ${refKey}`);
+            required.push(v);
 
-    return Object.assign({}, pick(baseSchema, ['$schema']), {
-      id: `${BASE_URL}/${messageCategory}/${type}.schema.json`,
-      type: 'object',
-      definitions: payload.definitions,
-      properties: {
-        type: { type: 'string', enum: [ type ] },
-        error: isException ?
-            { type: 'boolean', enum: [ true ] } : undefined,
-        payload: {
-          type: 'object',
-          properties: payload.properties,
-          required: payload.required,
+            return merge({}, memo, {
+              definitions: {
+                [refKey]: ref,
+              },
+              properties: {
+                [v]: { '$ref': `#/definitions/${refKey}` },
+              }
+            });
+          }, { definitions: {}, properties: {} });
+      payload.required = required;
+
+      return Object.assign({}, pick(baseSchema, ['$schema']), {
+        id: `${BASE_URL}/${messageCategory}/${type}.schema.json`,
+        type: 'object',
+        definitions: payload.definitions,
+        properties: {
+          type: { type: 'string', enum: [ type ] },
+          error: isException ?
+              { type: 'boolean', enum: [ true ] } : undefined,
+          payload: {
+            type: 'object',
+            properties: payload.properties,
+            required: payload.required,
+          },
         },
-      },
-      required: isException ?
-          ['payload', 'error', 'type'] : ['payload', 'type'],
-      commandConstructor: messageSchema.commandConstructor,
-    });
+        required: isException ?
+            ['payload', 'error', 'type'] : ['payload', 'type'],
+        commandConstructor: messageSchema.commandConstructor,
+      });
+    } catch (e) {
+      throw new Error(`
+
+  Unknown Error building message from file: ${filename}
+  ${e}`);
+
+    }
   };
 }
