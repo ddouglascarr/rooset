b928b447dbb521bbd501edf00805313efafdf0a7
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-04 10:52:02 +1100

    Exception bodies returned with Content-Type: application/json

--- rooset-http-api/src/main/java/org/rooset/httpapi/aop/HandleServiceErrorsAspect.java
+++ rooset-http-api/src/main/java/org/rooset/httpapi/aop/HandleServiceErrorsAspect.java
@@ -3,7 +3,9 @@ package org.rooset.httpapi.aop;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
@@ -20,17 +22,24 @@ public class HandleServiceErrorsAspect
         try {
             return (ResponseEntity<Object>) joinPoint.proceed();
         } catch (RatkException e) {
+            final HttpHeaders httpHeaders= new HttpHeaders();
+            httpHeaders.setContentType(MediaType.APPLICATION_JSON);
             switch (e.getExceptionCode()) {
                 case ITEM_NOT_FOUND_EXCEPTION:
-                    return new ResponseEntity<>(e.serialize().toString(), HttpStatus.NOT_FOUND);
+                    return new ResponseEntity<>(
+                        e.serialize().toString(), httpHeaders, HttpStatus.NOT_FOUND);
                 case CONFLICT_EXCEPTION:
-                    return new ResponseEntity<>(e.serialize().toString(), HttpStatus.CONFLICT);
+                    return new ResponseEntity<>(
+                        e.serialize().toString(), httpHeaders, HttpStatus.CONFLICT);
                 case UNPRIVILEGED_EXCEPTION:
-                    return new ResponseEntity<>(e.serialize().toString(), HttpStatus.UNAUTHORIZED);
+                    return new ResponseEntity<>(
+                        e.serialize().toString(), httpHeaders, HttpStatus.UNAUTHORIZED);
                 case UNAUTHORIZED_EXCEPTION:
-                    return new ResponseEntity<>(e.serialize().toString(), HttpStatus.UNAUTHORIZED);
+                    return new ResponseEntity<>(
+                        e.serialize().toString(), httpHeaders, HttpStatus.UNAUTHORIZED);
                 default:
-                    return new ResponseEntity<>(e.serialize().toString(), HttpStatus.INTERNAL_SERVER_ERROR);
+                    return new ResponseEntity<>(
+                        e.serialize().toString(), httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
             }
         } catch (Throwable e) {
             e.printStackTrace();
