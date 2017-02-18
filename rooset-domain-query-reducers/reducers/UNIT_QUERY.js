a7d3dae3503e2c566981719583dda076db7fa4bf
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-18 13:39:04 +1100

    UNIT_QUERY test passing

--- rooset-domain-query-reducers/reducers/UNIT_QUERY.js
+++ rooset-domain-query-reducers/reducers/UNIT_QUERY.js
@@ -16,6 +16,32 @@ fromAll()
     s.id = e.body.payload.id;
     s.name = e.body.payload.name;
     s.description = e.body.payload.description;
-  }
+  },
+
+  UNIT_POLICY_SET_EVENT: function(s, e) {
+    const p = e.body.payload;
+    const policy = {
+      policyId: p.policyId,
+      name: p.name,
+      description: p.description,
+      discussionTime: p.discussionTime,
+      verificationTime: p.verificationTime,
+      votingTime: p.votingTime,
+      issueQuorumNum: p.issueQuorumNum,
+      issueQuorumDen: p.issueQuorumDen,
+    };
+    s.policy = policy;
+  },
+
+  AREA_CREATED_EVENT: function(s, e) {
+    const p = e.body.payload;
+    const area = {
+      areaId: p.areaId,
+      name: p.name,
+      description: p.description,
+      externalReference: p.externalReference,
+    };
+    s.areas.push(area);
+  },
 
 });
