1facf43a0ef3236cfaca846b1d7167cf8bd71fb7
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-23 12:48:29 +1100

    #26 login entrypoint (#27)
      - POST to /login with credentials to get a cookie
      - Keeping basic auth for now (the tests rely on it)

---
+++ rooset-http-api/src/main/java/org/rooset/httpapi/config/RestAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package org.rooset.httpapi.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint
+{
+
+  @Override
+  public void commence(
+      HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse,
+      AuthenticationException e)
+      throws IOException, ServletException
+  {
+    httpServletResponse.sendError(
+        HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+  }
+
+}
---
+++ rooset-http-api/src/main/java/org/rooset/httpapi/config/RestAuthenticationFailureHandler.java
@@ -0,0 +1,10 @@
+package org.rooset.httpapi.config;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RestAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler
+{
+
+}
