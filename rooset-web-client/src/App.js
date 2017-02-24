b65325c3b2befb0768b3c78c5bc31e25e1833fee
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-24 11:02:08 +1100

    route change event route

--- rooset-web-client/src/App.js
+++ rooset-web-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import ProjectNavbar from "./containers/ProjectNavbar";
-import HomePage from './containers/HomePage';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./containers/HomePage";
+import LoginPage from "./containers/LoginPage";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -9,7 +10,31 @@ class App extends Component {
       <Router>
         <div className="App">
           <ProjectNavbar />
-          <Route exact path="/" component={HomePage} />
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route
+              exact
+              path="/login"
+              component={LoginPage}
+              onEnter={(nextState, replace, cb) => {
+                console.log(nextState);
+                console.log("foobar");
+                cb();
+              }}
+              componentDidUpdate={() => console.log("login did mount")}
+            />
+            <Route path="/" render={() => <div>404</div>} />
+          </Switch>
+
+          {/* for dispatching redux route events */}
+          <Route
+            path="/"
+            render={(props) => {
+              console.log("path changed");
+              console.log(props.location.pathname);
+              return null;
+            }}
+          />
         </div>
       </Router>
     );
---
+++ rooset-web-client/src/containers/LoginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class LoginPage extends React.Component {
+  render() {
+    return <div><p>Login Page</p></div>;
+  }
+}
