2e8248275f40da7905d0a4f56f761384dd3230e0
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-25 21:56:05 +1100

    added HttpAction to Action type

--- rooset-web-client/src/actions/Action.js
+++ rooset-web-client/src/actions/Action.js
@@ -3,4 +3,5 @@
 import type { PageAction } from "./PageAction";
 import type { QueryAction } from "./http/QueryActions";
 
+export type HttpAction = QueryAction; // | CommandAction;
 export type Action = PageAction | QueryAction;
