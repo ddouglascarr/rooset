24be716654d7f325a879a011cf597029a8a38fa5
Daniel Carr <d.douglas.carr@gmail.com>
2017-02-27 19:29:07 +1100

    query test passing

--- rooset-application-toolkit/ratk-declarations-utils/src/applyUriTemplate.js
+++ rooset-application-toolkit/ratk-declarations-utils/src/applyUriTemplate.js
@@ -1,7 +1,14 @@
 const { template } = require('lodash');
 
 module.exports = (uriTmpl, vars) => {
-  const opts = { interpolate:  /\{(.+?)\}/g };
-  const tmpl = template(uriTmpl, opts);
-  return tmpl(vars);
+  try {
+    const opts = { interpolate:  /\{(.+?)\}/g };
+    const tmpl = template(uriTmpl, opts);
+    return tmpl(vars);
+  } catch (err) {
+    throw new Error(`
+      applyUriTemplate failed with error: ${err.message}.
+      uriTmpl: ${uriTmpl}
+      vars: ${JSON.stringify(vars, null, 2)}`);
+  }
 }
