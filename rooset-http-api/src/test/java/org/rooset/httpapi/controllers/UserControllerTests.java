fe5307949837d4738bfad41a5a219f755af73d88
Daniel Carr <d.douglas.carr@gmail.com>
2017-03-03 13:26:08 +1100

    Session tests passing

--- rooset-http-api/src/test/java/org/rooset/httpapi/controllers/UserControllerTests.java
+++ rooset-http-api/src/test/java/org/rooset/httpapi/controllers/UserControllerTests.java
@@ -1,6 +1,8 @@
 package org.rooset.httpapi.controllers;
 
 import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.rooset.httpapi.models.UserModel;
@@ -9,9 +11,9 @@ import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -30,19 +32,33 @@ public class UserControllerTests
   @Autowired
   private UserRepository userRepository;
 
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
+  private UUID userId = UUID.randomUUID();
 
-  @Test
-  public void getUserTest()
+  @Before
+  public void start() throws Exception
   {
-    UUID userId = UUID.randomUUID();
     UserModel testUser = new UserModel(
         "testUser", "Test User",
         "Test", "User", "test@example.com",
-        "encryptedPassword");
+        passwordEncoder.encode("password1"));
     testUser.setId(userId);
     userRepository.save(testUser);
+  }
 
+
+
+  @After
+  public void end() throws Exception
+  {
+    userRepository.delete(userId);
+  }
+
+  @Test
+  public void getUserTest() throws Exception
+  {
     JSONObject expectedHttpRespBody = new JSONObject();
     expectedHttpRespBody.put("id", userId.toString());
     expectedHttpRespBody.put("username", "testUser");
@@ -57,6 +73,38 @@ public class UserControllerTests
     JSONAssert.assertEquals(expectedHttpRespBody, responseBody, true);
   }
 
+
+
+  @Test
+  public void getSessionShould401WhenUnauthenticated() throws Exception
+  {
+    ResponseEntity<String> response =  this.restTemplate.getForEntity(
+        "/session", String.class);
+
+    assertEquals("should be 401, was: " + response.getStatusCode().toString(),
+        response.getStatusCode(), HttpStatus.UNAUTHORIZED);
+  }
+
+
+
+  @Test
+  public void getSessionTest()
+  {
+
+    JSONObject expectedRespBody = new JSONObject();
+    expectedRespBody.put("id", userId.toString());
+    expectedRespBody.put("username", "testUser");
+    expectedRespBody.put("displayName", "Test User");
+
+    ResponseEntity<String> response = this.restTemplate
+        .withBasicAuth("testUser", "password1")
+        .getForEntity("/session", String.class);
+    JSONObject responseBody = new JSONObject(response.getBody());
+
+    assertTrue("2xx expected, got " + response.getStatusCode().toString(),
+        response.getStatusCode().is2xxSuccessful());
+    JSONAssert.assertEquals(expectedRespBody, responseBody, true);
+  }
 
 
 }
