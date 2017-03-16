12d01a46af156d0580c13e8070b2a321802e88fc
Daniel Carr <d.douglas.carr@gmail.com>
2017-03-16 15:54:13 +1100

    one test failing for some reason

--- rooset-domain-query-reducers/reducers/UNIT_DELEGATIONS_QUERY.js
+++ rooset-domain-query-reducers/reducers/UNIT_DELEGATIONS_QUERY.js
@@ -31,5 +31,23 @@ fromCategory("unitDelegations").foreachStream().when({
       trusteeId: p.trusteeId,
       blocked: false
     };
+  },
+
+  AREA_DELEGATION_UNSET_EVENT: (s, e) => {
+    const p = e.body.payload;
+    s.areas[p.areaId] = undefined;
+  },
+
+  DELEGATION_BLOCKED_FOR_AREA_EVENT: (s, e) => {
+    const p = e.body.payload;
+    s.areas[p.areaId] = {
+      trusteeId: null,
+      blocked: true
+    };
+  },
+
+  DELEGATION_UNBLOCKED_FOR_AREA_EVENT: (s, e) => {
+    const p = e.body.payload;
+    s.areas[p.areaId] = undefined;
   }
 });
