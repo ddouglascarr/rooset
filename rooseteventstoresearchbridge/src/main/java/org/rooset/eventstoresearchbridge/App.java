8d70a682ac071e448bb88f667e70f517d5b24c7e
Daniel Carr <d.douglas.carr@gmail.com>
2017-04-22 18:35:27 +1000

    refactor, save checkpoint

--- rooseteventstoresearchbridge/src/main/java/org/rooset/eventstoresearchbridge/App.java
+++ rooseteventstoresearchbridge/src/main/java/org/rooset/eventstoresearchbridge/App.java
@@ -18,9 +18,11 @@ import eventstore.j.EsConnection;
 import eventstore.j.EsConnectionFactory;
 import eventstore.util.ByteStringToString;
 
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.json.JSONObject;
 
@@ -37,35 +39,7 @@ public class App
     final ActorSystem system = ActorSystem.create();
     final EsConnection connection = EsConnectionFactory.create(system);
     final ElasticSearchService elasticSearchService = new ElasticSearchService();
+    elasticSearchService.subscribeToQueryStream("ISSUE_QUERY");
 
-    final Closeable closeable = connection.subscribeToStreamFrom("$projections-ISSUE_QUERY-result", new SubscriptionObserver<Event>()
-    {
-      public void onLiveProcessingStart(Closeable closeable)
-      {
-        system.log().info("live processing started");
-      }
-
-      public void onEvent(Event e, Closeable closeable)
-      {
-        system.log().info("Event happened");
-        system.log().info(e.data().data().value().decodeString("utf8"));
-        JSONObject json = new JSONObject(e.data().data().value().decodeString("utf8"));
-        final String id = json.getString("id");
-        elasticSearchService.prepareIndex("issue_query", "state", id, json);
-        system.log().info("processing " + e.data().eventId());
-        system.log().info("finished " + e.data().eventId());
-      }
-
-      public void onError(Throwable err)
-      {
-        system.log().error(err.toString());
-
-      }
-
-      public void onClose()
-      {
-        system.log().info("onClose");
-      }
-    },0, true, null);
   }
 }
--- rooseteventstoresearchbridge/src/main/java/org/rooset/eventstoresearchbridge/ElasticSearchService.java
+++ rooseteventstoresearchbridge/src/main/java/org/rooset/eventstoresearchbridge/ElasticSearchService.java
@@ -1,17 +1,29 @@
 package org.rooset.eventstoresearchbridge;
 
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Response;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.json.JSONObject;
 
+import java.io.Closeable;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+
+import akka.actor.ActorSystem;
+import eventstore.Event;
+import eventstore.SubscriptionObserver;
+import eventstore.j.EsConnection;
+import eventstore.j.EsConnectionFactory;
 
 public class ElasticSearchService
 {
   private TransportClient client;
+  private ActorSystem system = ActorSystem.create();
+  private EsConnection connection = EsConnectionFactory.create(system);
 
   public ElasticSearchService()
   {
@@ -24,12 +36,61 @@ public class ElasticSearchService
     }
   }
 
-
-  public void prepareIndex(String index, String type, String id, JSONObject body)
+  public IndexResponse prepareIndex(String index, String type, String id, JSONObject body)
   {
-    client.prepareIndex(index, type, id)
+    IndexResponse response = client.prepareIndex(index, type, id)
         .setSource(body.toString())
         .get();
+    return response;
+  }
+
+  private Integer getEventStoreCheckpoint(String queryName)
+  {
+    return 0;
+  }
+
+  private String parseQueryName(String queryName)
+  {
+    return queryName.toLowerCase();
+  }
+
+  private void setEventStoreCheckpoint(String queryName, Integer eventStoreNumber)
+  {
+    client.prepareIndex(parseQueryName(queryName), "checkpoint")
+        .setSource(new JSONObject().put("checkpoint", eventStoreNumber).toString())
+        .get();
+  }
+
+  public Closeable subscribeToQueryStream(final String queryName)
+  {
+    return connection.subscribeToStreamFrom("$projections-" + queryName + "-result", new SubscriptionObserver<Event>()
+    {
+      public void onLiveProcessingStart(Closeable closeable)
+      {
+        system.log().info("live processing started");
+      }
+
+      public void onEvent(Event e, Closeable closeable)
+      {
+        JSONObject json = new JSONObject(e.data().data().value().decodeString("utf8"));
+        final String id = json.getString("id");
+        IndexResponse resp = prepareIndex(parseQueryName(queryName), "state", id, json);
+        system.log().info(new Boolean(resp.status().toString() == "OK").toString());
+        system.log().info("finished " + e.data().eventId());
+        setEventStoreCheckpoint(queryName, e.number().value());
+      }
+
+      public void onError(Throwable err)
+      {
+        system.log().error(err.toString());
+
+      }
+
+      public void onClose()
+      {
+        system.log().info("ElasticSearch  connection closed");
+      }
+    }, getEventStoreCheckpoint(queryName), true, null);
   }
 
   public void close() {
