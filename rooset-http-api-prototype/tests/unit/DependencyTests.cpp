0c0deab97ba4a31e9cba1afdd455927271a49d07
Daniel Carr <d.douglas.carr@gmail.com>
2017-05-20 22:15:14 +1000

    test test

--- rooset-http-api-prototype/tests/unit/DependencyTests.cpp
+++ rooset-http-api-prototype/tests/unit/DependencyTests.cpp
@@ -2,4 +2,19 @@
 
 namespace roosetHttpTesting {
 
+
+
+TEST(dependencies, test_test_should_pass)
+{
+  EXPECT_EQ(true, true);
+}
+
+
+
+TEST(dependencies, test_test_should_fail)
+{
+  EXPECT_EQ(true, false);
+}
+
+
 }
