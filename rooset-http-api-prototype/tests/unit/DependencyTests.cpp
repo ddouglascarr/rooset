1e648839c26220823e430f9c3c2777930d054c9d
Daniel Carr <d.douglas.carr@gmail.com>
2017-05-20 22:46:24 +1000

    r3 dependency test

--- rooset-http-api-prototype/tests/unit/DependencyTests.cpp
+++ rooset-http-api-prototype/tests/unit/DependencyTests.cpp
@@ -1,6 +1,9 @@
 #include "gtest/gtest.h"
 
-namespace roosetHttpTesting {
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <r3/r3.h>
 
 
 
@@ -11,10 +14,34 @@ TEST(dependencies, test_test_should_pass)
 
 
 
-TEST(dependencies, test_test_should_fail)
+TEST(dependencies_lib3, should_match_route)
 {
-  EXPECT_EQ(true, false);
+  auto *n = r3_tree_create(10);
+  int routeData1 = 1;
+  int routeData2 = 2;
+  std::string routeFoo = "/foo";
+  std::string routeFooX = "/foo/{x}";
+  
+  r3_tree_insert_pathl(n, routeFoo.c_str(), routeFoo.length(), &routeData1);
+  r3_tree_insert_pathl(n, routeFooX.c_str(), routeFooX.length(), &routeData2);
+
+  char *errstr = nullptr;
+  int err = r3_tree_compile(n, &errstr);
+  if (err != 0) {
+    std::cout << errstr << "\n";
+    free(errstr);
+  }
+  EXPECT_EQ(err, 0);
+
+  auto *matchedNode = r3_tree_matchl(n, "/foo", strlen("/foo"), NULL);
+  EXPECT_TRUE(matchedNode);
+  EXPECT_EQ((*(int*) matchedNode->data), 1);
+
+  auto *matchedNodeBar = r3_tree_matchl(n, "/foo/abcd", strlen("/foo/abcd"), NULL);
+  EXPECT_TRUE(matchedNodeBar);
+  EXPECT_EQ((*(int*) matchedNodeBar->data), 2);
+
+  auto* unmatched = r3_tree_matchl(n, "/bar", strlen("/bar"), NULL);
+  EXPECT_FALSE(unmatched);
 }
 
-
-}
