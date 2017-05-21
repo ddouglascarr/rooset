0256ffcc363e333b9ce8ea9664e023553af0f6bd
Daniel Carr <d.douglas.carr@gmail.com>
2017-05-21 16:07:53 +1000

    start and stop test passing

--- rooset-http-api-prototype/tests/integration/RoutingTests.cpp
+++ rooset-http-api-prototype/tests/integration/RoutingTests.cpp
@@ -1,8 +1,24 @@
 #include "gmock/gmock.h"
 
+#include <future>
+#include "http-server/Server.h"
+#include "EchoHandlerFactory.h"
 
 
-TEST(routing_tests, test_test_should_pass)
+
+TEST(routing_tests, server_should_start_and_stop)
 {
+  // starting server
+  rooset::Server<EchoHandlerFactory> server("localhost", 11005);
+  std::promise<void> p;
+  auto f = p.get_future();
+  std::thread t = server.start([&p](){ p.set_value(); });
+  
+  // wait for server to be ready before starting tests
+  f.get();
   EXPECT_EQ(true, true);
+
+  // kill the server, and wait for the thread to close
+  server.stop();
+  t.join();
 }
