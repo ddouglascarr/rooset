2547f662560345a771a2363c55fecaa1571a5592
Daniel Carr <d.douglas.carr@gmail.com>
2017-05-21 14:03:39 +1000

    Server class compiles

---
+++ rooset-http-api-prototype/include/http-server/Server.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <vector>
+#include <folly/Memory.h>
+#include <folly/io/async/EventBaseManager.h>
+#include <proxygen/httpserver/HTTPServer.h>
+#include <proxygen/httpserver/RequestHandlerFactory.h>
+#include <unistd.h>
+
+
+
+namespace rooset {
+
+using folly::EventBase;
+using folly::EventBaseManager;
+using folly::SocketAddress;
+
+
+
+template<typename H>
+class Server
+{
+private:
+  std::vector<proxygen::HTTPServer::IPConfig> ipConfigs;
+  proxygen::HTTPServer s;
+
+public:
+
+
+
+  inline Server(std::string serverAddress, int port) :
+      s(generateOptions(sysconf(_SC_NPROCESSORS_ONLN)))
+  {
+    ipConfigs = {
+      { 
+        folly::SocketAddress(serverAddress, port, true),
+        proxygen::HTTPServer::Protocol::HTTP
+      },
+    };
+
+  }
+
+
+
+  ~Server() {}
+
+
+
+  std::thread start()
+  {
+    s.bind(ipConfigs);
+    std::thread t([&] () {
+      s.start();
+    });
+
+    return t;
+  }
+
+
+
+  void stop()
+  {
+    s.stop();
+  }
+
+
+private:
+
+
+
+  proxygen::HTTPServerOptions generateOptions(int threads)
+  {
+    proxygen::HTTPServerOptions options;
+    options.threads = static_cast<size_t>(threads);
+    options.idleTimeout = std::chrono::milliseconds(60000);
+    options.shutdownOn = {SIGINT, SIGTERM};
+    options.enableContentCompression = false;
+    options.handlerFactories = proxygen::RequestHandlerChain()
+        .addThen<H>()
+        .build();
+    options.h2cEnabled = true;
+    return options;
+  }
+
+};
+
+
+
+}
