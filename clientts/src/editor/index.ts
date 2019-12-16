ae1a27f28757ef57f82813122d956d78321b5e11
Daniel Carr <d.douglas.carr@gmail.com>
2019-12-16 14:22:26 +1100

    undo, redo

--- clientts/src/editor/index.ts
+++ clientts/src/editor/index.ts
@@ -1,9 +1,21 @@
 import {schema} from 'prosemirror-schema-basic';
 import {EditorState} from 'prosemirror-state';
 import {EditorView} from 'prosemirror-view';
+import {undo, redo, history} from "prosemirror-history"
+import {keymap} from "prosemirror-keymap"
 
 export const initProsemirror = () => {
-  const state = EditorState.create({schema});
+  const state = EditorState.create({
+    schema,
+    plugins: [
+      history(),
+      keymap({
+        'Mod-z': undo,
+        'Mod-y': redo,
+      }),
+    ],
+  });
+
   new EditorView(document.getElementById('initiative-editor')!, {
     state,
   });
