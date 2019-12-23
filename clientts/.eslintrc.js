41784c9727170be8634119427557ec407613cbe8
Daniel Carr <d.douglas.carr@gmail.com>
2019-12-23 21:33:11 +1100

    build multiple bundles

--- clientts/.eslintrc.js
+++ clientts/.eslintrc.js
@@ -20,10 +20,6 @@ module.exports = {
         ignoreRestSiblings: false,
       },
     ],
-    "@typescript-eslint/explicit-function-return-type": {
-      allowExpressions: false,
-      allowTypedFunctionExpressions: true,
-      allowHigherOrderFunctions: true,
-    },
+    '@typescript-eslint/explicit-function-return-type': 0,
   },
 };
--- clientts/webpack.client.config.js
+++ clientts/webpack.client.config.js
@@ -1,29 +1,60 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const isProd = process.env.NODE_ENV === 'production';
 
-
-module.exports = {
-  entry: './src/entry.tsx',
-  mode: isProd ? 'production' : 'development',
-  devtool: isProd ? 'cheap-module-source-map' : 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: '../lffrontend/static/js',
+module.exports = [
+  {
+    entry: './src/entry.tsx',
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd
+      ? 'cheap-module-source-map'
+      : 'cheap-module-eval-source-map',
+    // webpack dev server only reads the first one of these in the array of configs
+    devServer: {
+      contentBase: './dist',
+      writeToDisk: true, // so we can watch the serverside ones on the disk
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      modules: ['node_modules', 'src'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
+  {
+    target: 'node',
+    entry: './src/gitService/index.ts',
+    mode: isProd ? 'production' : 'development',
+    devtool: isProd
+      ? 'cheap-module-source-map'
+      : 'cheap-module-eval-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      modules: ['node_modules', 'src'],
+    },
+    output: {
+      filename: 'gitService.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
   },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    modules: ['node_modules', 'src'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, '..', 'lffrontend', 'static', 'js'),
-  },
-};
+];
